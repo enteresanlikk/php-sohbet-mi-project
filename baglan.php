6ac1866edf9fca61cd7bba0e01ae58333cb82b29
enteresanlikk <zeki1198@gmail.com>
2016-11-11 10:45:32 +0200

    sdf

--- baglan.php
+++ baglan.php
@@ -1,8 +1,8 @@
 ﻿<?php
 	session_start();
 	ob_start();
-	@$bag=mysql_connect("mysql.hostinger.web.tr","u239346644_root","bilal61")or die("Bağlantı Başarısız!");
-	@$sec=mysql_select_db("u239346644_soh",$bag)or die("Veritabanı Yok!");
+	@$bag=mysql_connect("Adres","Veritabanı Kullanıcı Adı","Veritabanı Şifresi")or die("Bağlantı Başarısız!");
+	@$sec=mysql_select_db("Veritabanı İsimi",$bag)or die("Veritabanı Yok!");
 	mysql_query("SET NAMES utf8");
 	mysql_query("SET CHARACTER SET utf8");
 	error_reporting(0);
